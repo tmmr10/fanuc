4f2d145bc3bef247c2d8f81f373f559a32bc47d7
tmmr <tom.maier@zeitgaist.com>
2023-06-27 15:46:41 +0200

    add docs to detector.py

--- gesture/detector.py
+++ gesture/detector.py
@@ -8,9 +8,13 @@ from cvzone.HandTrackingModule import HandDetector
 WIDTH, HEIGHT = 1280, 720
 
 class Detector:
+    """
+    A class for detecting hand gestures using a webcam.
+    """
+
     def __init__(self):
         cap = cv2.VideoCapture(0)
-        # For mac we had to use different Camera Device
+        # For Mac, we had to use a different camera device
         # cap = cv2.VideoCapture(1)
         print("Video Captured")
         cap.set(3, WIDTH)
@@ -24,11 +28,16 @@ class Detector:
         self.hands_lock = threading.Lock()
 
     def start(self):
-        # Starte den Webcam-Stream in einem separaten Thread
+        """
+        Start the webcam stream in a separate thread.
+        """
         webcam_thread = threading.Thread(target=self.run)
         webcam_thread.start()
 
     def run(self):
+        """
+        Run the webcam stream and hand gesture detection.
+        """
         webcam_thread = threading.Thread(target=self._detect_gesture)
         webcam_thread.start()
 
@@ -46,6 +55,9 @@ class Detector:
         webcam_thread.join()
 
     def _detect_gesture(self):
+        """
+        Perform hand gesture detection.
+        """
         try:
             while self.is_running:
                 with self.hands_lock:
@@ -56,15 +68,20 @@ class Detector:
                     elif len(hands_copy) == 2:
                         self.two_hand_detection(hands_copy)
         except Exception as e:
-            print("An error occured while detecting hand gesture.", e)
-            print("Press q to end programm.")
-            print("Error...retry...(Wait for 5 seconds)")
+            print("An error occurred while detecting hand gesture:", e)
+            print("Press 'q' to end the program.")
+            print("Error... retry... (Waiting for 5 seconds)")
             time.sleep(5)
             self._detect_gesture()
 
-    # Hier wird auf gesten reagiert, die nur funktionieren, wenn eine hand erkannt wurde
     def one_hand_detection(self, hands_copy):
-        # Get first hand (only one activated)
+        """
+        Detect gestures that work only when one hand is detected.
+
+        Args:
+            hands_copy (list): A list containing the detected hand(s).
+        """
+        # Get the first hand (only one activated)
         hand = hands_copy[0]
         fingers = self.detector.fingersUp(hand)
         # print(fingers)
@@ -72,9 +89,14 @@ class Detector:
         if fingers == [1, 1, 0, 0, 0]:
             self.one_index_finger_up(hand)
 
-   # Hier wird auf gesten reagiert, die nur funktionieren, wenn zwei Hände erkannt wurden
     def two_hand_detection(self, hands_copy):
-        # Get two hands
+        """
+        Detect gestures that work only when two hands are detected.
+
+        Args:
+            hands_copy (list): A list containing the detected hand(s).
+        """
+        # Get the two hands
         hand1, hand2 = hands_copy
         fingers1 = self.detector.fingersUp(hand1)
         fingers2 = self.detector.fingersUp(hand2)
@@ -84,10 +106,30 @@ class Detector:
             self.two_all_fingers_down(hand1, hand2)
 
     def one_index_finger_up(self, hand):
+        """
+        Handle the gesture when one index finger is up.
+
+        Args:
+            hand: The detected hand.
+        """
         pass
 
     def two_index_fingers_up(self, hand1, hand2):
+        """
+        Handle the gesture when two index fingers are up.
+
+        Args:
+            hand1: The first detected hand.
+            hand2: The second detected hand.
+        """
         pass
 
     def two_all_fingers_down(self, hand1, hand2):
+        """
+        Handle the gesture when all fingers are down for both hands.
+
+        Args:
+            hand1: The first detected hand.
+            hand2: The second detected hand.
+        """
         pass
